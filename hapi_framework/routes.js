12f8a58c4ba3a860a538d27c5a68e361706f40dd
Novebri Tito Ramadhani <97221880+Benedixx@users.noreply.github.com>
2024-03-30 00:43:58 +0700

    Update: path parameters hello

--- hapi_framework/routes.js
+++ hapi_framework/routes.js
@@ -27,7 +27,15 @@ const routes = [
         method: `*`,
         path: `/{any*}`,
         handler: (request, h) => `BAD REQUEST: ${request.method} ${request.url}`
-    }
+    },
+    {
+        method: 'GET',
+        path: '/hello/{name?}',
+        handler: (request, h) => {
+            const {name = 'stranger'} = request.params;
+            return `Hello, ${name}!`;
+        }
+    },
 ];
 
 module.exports = routes;
