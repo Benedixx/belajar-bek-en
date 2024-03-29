6dc4d785d48cc07b3fb9483dae104f61936a6ba7
Novebri Tito Ramadhani <97221880+Benedixx@users.noreply.github.com>
2024-03-29 23:34:15 +0700

    Update: body request

--- nodeJS-webServer/server.js
+++ nodeJS-webServer/server.js
@@ -3,7 +3,25 @@ const http = require('http');
 const requestListener = (request, response) => {
     response.setHeader('Content-Type', 'text/html');
     response.statusCode = 200;
-    response.end('<h1>Hello World</h1>');
+    
+    const {method} = request;
+
+    if(method == 'GET') {
+        response.end('getting the data');
+    }
+    if (method == 'POST') {
+        let body = [];
+
+        request.on('data', (chunk) => {
+            body.push(chunk);
+        })
+
+        request.on('end', () => {
+            body = Buffer.concat(body).toString();
+            const {name} = JSON.parse(body);
+            response.end(`<h1>Hai, ${name}!</h1>`);
+        })
+    }
 };
 
 const server = http.createServer(requestListener);
